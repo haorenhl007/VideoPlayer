2bed574e4d7d30df3026a67952742e934e226f90
Zgjian <1819536942@qq.com>
2017-10-30 19:19:12 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <QApplication>
+#include <QTextCodec>
+
+#include "videoplayer/videoplayer.h"
+#include "CustomTitleWidget/myhelper.h"
+
+#undef main
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+//    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+//    QTextCodec::setCodecForLocale(codec);
+//    QTextCodec::setCodecForCStrings(codec);
+//    QTextCodec::setCodecForTr(codec);
+
+    myHelper::SetUTF8Code();
+//    myHelper::SetStyle("black");//黑色风格
+    myHelper::SetStyle("blue");//蓝色风格
+//    myHelper::SetStyle("gray");//灰色风格
+//    myHelper::SetStyle("navy");//天蓝色风格
+    myHelper::SetChinese();
+
+    VideoPlayer w;
+    w.show();
+
+    return a.exec();
+}
